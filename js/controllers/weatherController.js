7e51598a49f05bd6044434a1b32242e5dfd89d46
kovalenkovpu <t085en@gmail.com>
2017-06-22 16:49:01 +0300

    SPA с openweathermap API

--- js/controllers/weatherController.js
+++ js/controllers/weatherController.js
@@ -1,5 +1,24 @@
-app.controller('weatherController', ['$scope', '$routeParams', 'weatherData', function($scope, $routeParams, weatherData) {
-	weatherData.getData.then(function(data) {
-		$scope.data = data;
+app.controller('weatherController', ['$scope', '$routeParams', 'weatherData', '$resource', function($scope, $routeParams, weatherData, $resource) {
+	
+	let api = weatherData.api;
+	
+	$scope.city = weatherData.city;
+
+	$scope.$watch('city', () => {
+		weatherData.city = $scope.city;
 	});
+
+	$scope.resourceAPI = $resource(api, { get: {method: "JSONP"}});
+
+	$scope.getCurrentForecast = function() {
+		$scope.httpData = $scope.resourceAPI.get({ q: $scope.city});
+	};
+
+	$scope.convertTemp = function(temp) {
+		return (temp - 273.15).toFixed(2);
+	};
+	
+	$scope.convertDate = function(date) {
+		return new Date(date * 1000);
+	};
 }]);
