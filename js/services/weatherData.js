551d01c324699fd3cf0a93daef6d793d78b010a6
kovalenkovpu <t085en@gmail.com>
2017-06-22 09:59:22 +0300

    Исправление ошибки доступа к данным

--- js/services/weatherData.js
+++ js/services/weatherData.js
@@ -1,5 +1,5 @@
 app.factory('weatherData', ['$http', function($http) {
-	let dataPromise = $http.get('data/data.json')
+	let dataPromise = $http.get('./data/data.json')
 		.then
 			(function(response) {
     			return response.data;
