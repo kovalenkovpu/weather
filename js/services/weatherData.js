055322071987b583e66b8d1d15b402d86271d9d8
kovalenkovpu <t085en@gmail.com>
2017-06-22 22:21:59 +0300

    Небольшие правки

--- js/services/weatherData.js
+++ js/services/weatherData.js
@@ -1,4 +1,4 @@
-app.service('weatherData', ['$http', function($http) {
+app.service('weatherData', [function() {
 	this.api = '//api.openweathermap.org/data/2.5/weather?APPID=f014a10f31104549ece104488875b27b';
 
 	this.apiFiveDay = '//api.openweathermap.org/data/2.5/forecast?APPID=f014a10f31104549ece104488875b27b';
